648ec93fe5baa6728810b33106283864ecacaf35
Jamie Hall <the.sly.marbo@googlemail.com>
2013-05-01 13:55:29 +0100

    Added unidirectional flag support

--- stream.go
+++ stream.go
@@ -1,20 +1,21 @@
 package spdy
 
 import (
-  // "crypto/tls"
+// "crypto/tls"
 //   "net"
 //   "sync"
 )
 
 type stream struct {
-  conn         *connection
-  streamID     uint32
-  state        StreamState
-  priority     uint8
-  certificates [][]byte
-  headers      *Headers
-  settings     []*Setting
-  credentials  []Certificate
+  conn           *connection
+  streamID       uint32
+  state          StreamState
+  priority       uint8
+  certificates   [][]byte
+  headers        *Headers
+  settings       []*Setting
+  credentials    []Certificate
+  unidirectional bool
 }
 
 type StreamState uint8
