a82815e14ab883112b8330ad45498e2768378279
James Abley <james.abley@gmail.com>
2015-03-02 19:35:50 +0000

    Fixes #78 – resource leak with streams
    Not sure whether this is the correct fix, but it contains a failing test
    which now passes.
    
    Given a server which specifies a finite max number of streams, we see
    the problem.
    
    This change ensures that the requestStreamLimit is managed
    appropriately.

--- client_test.go
+++ client_test.go
@@ -14,6 +14,7 @@ import (
 	"testing"
 
 	"github.com/SlyMarbo/spdy"
+	"github.com/SlyMarbo/spdy/common"
 )
 
 func init() {
@@ -37,6 +38,28 @@ func TestClient(t *testing.T) {
 		t.Error(err)
 	} else if s := string(b); !strings.HasPrefix(s, "User-agent:") {
 		t.Errorf("Incorrect page body (did not begin with User-agent): %q", s)
+	}
+}
+
+func TestClientClosesResources(t *testing.T) {
+	defer afterTest(t)
+	ts := newServer(robotsTxtHandler)
+	defer ts.Close()
+
+	client := newClient()
+
+	for i := 0; i < common.DEFAULT_STREAM_LIMIT+1; i++ {
+		r, err := client.Get(ts.URL)
+		var b []byte
+		if err == nil {
+			b, err = pedanticReadAll(r.Body)
+			r.Body.Close()
+		}
+		if err != nil {
+			t.Error(err)
+		} else if s := string(b); !strings.HasPrefix(s, "User-agent:") {
+			t.Errorf("Incorrect page body (did not begin with User-agent): %q", s)
+		}
 	}
 }
 
